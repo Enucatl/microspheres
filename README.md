e7a94e0b0a952d4c833ef598208bdd8331f84dc0
Matteo Abis <gmatteo..abis@gmail.com>
2015-07-01 14:47:27 +0200

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# microspheres
+microspheres
+------------
 analysis of the microsphere dark field
+
+git hook to push the compiled version to gh-pages
+-------------------------------------------------
+```
+ln -s ../../pre-push .git/hooks/pre-push 
+```
---
+++ config.rb
@@ -0,0 +1,100 @@
+###
+# Compass
+###
+
+# Change Compass configuration
+# compass_config do |config|
+#   config.output_style = :compact
+# end
+
+###
+# Page options, layouts, aliases and proxies
+###
+
+# Per-page layout changes:
+#
+# With no layout
+# page "/path/to/file.html", :layout => false
+#
+# With alternative layout
+# page "/path/to/file.html", :layout => :otherlayout
+#
+# A path which all have the same layout
+# with_layout :admin do
+#   page "/admin/*"
+# end
+
+# Proxy pages (http://middlemanapp.com/basics/dynamic-pages/)
+# proxy "/this-page-has-no-template.html", "/template-file.html", :locals => {
+#  :which_fake_page => "Rendering a fake page with a local variable" }
+
+###
+# Helpers
+###
+
+# Automatic image dimensions on image_tag helper
+# activate :automatic_image_sizes
+
+# Reload the browser automatically whenever files change
+# configure :development do
+#   activate :livereload
+# end
+
+# Methods defined in the helpers block are available in templates
+# helpers do
+#   def some_helper
+#     "Helping"
+#   end
+# end
+
+set :css_dir, 'stylesheets'
+
+set :js_dir, 'javascripts'
+
+set :images_dir, 'images'
+
+set :haml, { :ugly => true }
+
+# Build-specific configuration
+configure :build do
+  # For example, change the Compass output style for deployment
+  activate :minify_css
+
+  # Minify Javascript on build
+  activate :minify_javascript
+
+  # Enable cache buster
+  # activate :asset_hash
+
+  # Use relative URLs
+  activate :relative_assets
+
+  # Or use a different image path
+  # set :http_prefix, "/Content/images/"
+end
+
+activate :deploy do |deploy|
+  deploy.method = :git
+  deploy.build_before = true # default: false
+  # Optional Settings
+  # deploy.remote   = 'custom-remote' # remote name or git url, default: origin
+  # deploy.branch   = 'custom-branch' # default: gh-pages
+  # deploy.strategy = :submodule      # commit strategy: can be :force_push or :submodule, default: :force_push
+  # deploy.commit_message = 'custom-message'      # commit message (can be empty), default: Automated commit at `timestamp` by middleman-deploy `version`
+end
+
+ignore "javascripts/vendor/d3.scatter/LICENSE"
+ignore "javascripts/vendor/d3.scatter/README.md"
+ignore "javascripts/vendor/d3.line.legend/LICENSE"
+ignore "javascripts/vendor/d3.line.legend/README.md"
+ignore "javascripts/vendor/d3.line/LICENSE"
+ignore "javascripts/vendor/d3.line/README.md"
+ignore "javascripts/vendor/d3.axes/LICENSE"
+ignore "javascripts/vendor/d3.axes/README.md"
+ignore "javascripts/vendor/d3.barchart/LICENSE"
+ignore "javascripts/vendor/d3.barchart/README.md"
+ignore "javascripts/vendor/d3.base.chart/LICENSE"
+ignore "javascripts/vendor/d3.base.chart/README.md"
+ignore "javascripts/vendor/MathJax/CONTRIBUTING.html"
+ignore "javascripts/vendor/MathJax/LICENSE"
+ignore "fonts/league_gothic_license"
