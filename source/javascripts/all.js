a52933b0b3db699a0b6a6052b4a3aee44dd5f24e
Matteo Abis <gmatteo..abis@gmail.com>
2015-07-02 12:13:43 +0200

    plot raw images

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -2,6 +2,7 @@
 //= require vendor/d3.base.chart/base.chart
 //= require vendor/d3.axes/axes
 //= require vendor/d3.image/image
+//= require vendor/d3.colorbar/colorbar
 //= require vendor/d3.barchart/bar
 //= require vendor/d3.scatter/scatter
 //= require vendor/d3.line/line
