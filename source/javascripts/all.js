8f2f64dbc8c866c935444b92dedefcdf0dd1ed87
Matteo Abis <gmatteo..abis@gmail.com>
2015-07-07 10:30:01 +0200

    rename base chart to avoid dots

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,5 +1,5 @@
 //= require _stylish-portfolio
-//= require vendor/d3.base.chart/base.chart
+//= require vendor/d3.base-chart/base-chart
 //= require vendor/d3.axes/axes
 //= require vendor/d3.image/image
 //= require vendor/d3.colorbar/colorbar
