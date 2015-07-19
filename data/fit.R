c583671031c630c794c71504a6f144ca1f1f10b8
Matteo Abis <gmatteo..abis@gmail.com>
2015-07-19 15:39:50 +0200

    add theoretical description

--- data/fit.R
+++ data/fit.R
@@ -30,7 +30,7 @@ mu = function(A, n_squared, D, e) {
     else {
         return(
             A * n_squared * e *
-            (x - sqrt(x^2 - 1) * (1 + 2 / x^2) +
+            (x - sqrt(x^2 - 1) * (1 + 1 / 2 / x^2) +
             (1 / x - 1 / 4 / x^3) *
             log((x + sqrt(x^2 - 1)) / (x - sqrt(x^2 - 1)))))
     }
